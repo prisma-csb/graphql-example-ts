7ab8e8528e6a241cbba3cfb61b24c1ceb6cd90b3
Nikolas Burk <nikolas.burk@gmail.com>
2019-01-15 18:32:42 +0100

    minor adjustments

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ import { resolvers } from './resolvers'
 import { defaultPlaygroundQuery } from './defaultPlaygroundQuery'
 
 /**
- * 🚀 Run `yarn start` to start the GraphQL server. 
- * 
+ * ▶️ Click the Play-button in the GraphQL Playground to run a query.
+ *  
  * 💡 To explore all available features, fork the CodeSandbox and connect it to your own demo database.
  * Follow the instructions here: https://github.com/prisma-csb/graphql-example-ts/blob/master/SETUP.md
  */
